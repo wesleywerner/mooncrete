ef3ebc790eed0fa864ce7cfd1416f157318ae238
Wesley Werner <wez@darknet.co.za>
2013-09-04 18:01:18 +0200

    removed blank line endings

--- mooncrete/eventmanager.py
+++ mooncrete/eventmanager.py
@@ -2,12 +2,12 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see http://www.gnu.org/licenses/.
 
@@ -82,3 +82,4 @@ class StateEvent(Event):
 
     def __str__(self):
         return ('%s: %s' % (self.name, str(self.state)))
+
