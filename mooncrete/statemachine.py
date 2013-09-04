6f2ad2044ad843cfae58059f2c6c8707871f535e
Wesley Werner <wez@darknet.co.za>
2013-09-04 18:01:50 +0200

    add state machine swap() call

--- mooncrete/statemachine.py
+++ mooncrete/statemachine.py
@@ -2,12 +2,12 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see http://www.gnu.org/licenses/.
 
@@ -27,24 +27,28 @@ class StateMachine(object):
     I've done so many of these what is writing one more from scratch :)
     Forgive me not commenting any further here, it is pretty self
     explanitory.
-    
+
     """
-    
+
     def __init__(self):
         self.stack = []
-    
+
     def peek(self):
         if not self.is_empty:
             return self.stack[-1]
-        
+
     def pop(self):
         if not self.is_empty:
             return self.stack.pop()
-        
+
     def push(self, value):
         self.stack.append(value)
         return value
-    
+
+    def swap(self, value):
+        self.pop()
+        self.push(value)
+
     @property
     def is_empty(self):
         return len(self.stack) == 0
