a1f1238beecf68d252bc0a7018aeb90fa5e93b83
Wesley Werner <wez@darknet.co.za>
2013-09-15 11:55:00 +0200

    add gray as a color

--- mooncrete/color.py
+++ mooncrete/color.py
@@ -2,12 +2,12 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see http://www.gnu.org/licenses/.
 
@@ -15,3 +15,4 @@ blue = (0, 0, 128)
 black = (0, 0, 0)
 white = (255, 255, 255)
 magenta = (255, 0, 255)
+gray = (32, 32, 32)
