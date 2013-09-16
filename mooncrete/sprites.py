51e09153d3298fa51869b5304bd95eb6a55b6e56
Wesley Werner <wez@darknet.co.za>
2013-09-16 23:48:11 +0200

    preliminary Turret sprite class to handle sprites with a angleable pointy-like turret piece

--- mooncrete/sprites.py
+++ mooncrete/sprites.py
@@ -111,6 +111,123 @@ class MooncreteSprite(pygame.sprite.Sprite):
         self.destination = position
 
 
+class TurretSprite(pygame.sprite.Sprite):
+    """
+    A sprite with base images and a angleable turret.
+
+    #   have multiple base images
+    #   have multiple turret images
+    #   have an angle which to rotate the turret
+    #   limit the angle with a min and max
+    #   a way to cycle the angle between a range
+    #   a way to override the angle
+    #   a way to animate base images with optional loop
+    #   a way to animate turret images with optional loop
+    #   a way to set the current base image
+    #
+    #   RADARS will constantly cycle their angle and images.
+    #   animating base: possibly
+    #   animating turret: yes
+    #   auto angle: yes
+    #
+    #   GUN TURRETS will center their angle unless they are
+    #   the closest to the cursor, then their angle is overridden.
+    #   Their images will only animate once when told to, when a shot is fired.
+    #   animating base: yes, explicitly
+    #   animating turret: yes, explicitly
+    #   auto angle: yes, with override
+
+    """
+
+    def __init__(self, rect):
+
+        super(TurretSprite, self).__init__()
+        self.name = 'turret'
+        # the position to draw the current image
+        self.rect = rect
+        # the current image to draw with
+        self.image = None
+
+        # have multiple base images
+        self.base_images = []
+        self.base_images_current = 0
+        # a way to animate base images with optional loop
+        self.base_images_loop = 0
+
+        # have multiple turret images
+        self.turret_images = []
+        self.turret_images_current = 0
+        # a way to animate turret images with optional loop
+        self.turret_images_loop = 0
+
+        # have an angle which to rotate the turret
+        self.turret_angle = 0
+        # limit the angle with a min and max
+        self.turret_angle_range = (0, 360)
+        # a way to cycle the angle between a range
+        self.turret_angle_auto_cycle = 0
+        # a way to override the angle
+        self.turrent_angle_override = -1
+
+
+        self.fps = 10
+        self._delay = 1000 / self.fps
+        self._last_update = 0
+
+    def subsurface_base_images(self, surface, rect_list):
+        """
+        Set all the base images from a list of rects and a source surface.
+
+        """
+
+        self.base_images = []
+        for rect in rect_list:
+            self.base_images.append(surface.subsurface(rect))
+
+    def subsurface_turret_images(self, surface, rect_list):
+        """
+        Set all the turret images from a list of rects and a source surface.
+
+        """
+
+        self.turret_images = []
+        for rect in rect_list:
+            self.turret_images.append(surface.subsurface(rect))
+
+    def canupdate(self, t):
+        """
+        Tests if it is time to update again
+        time is the current game ticks. It is used to calculate when to update
+            so that animations appear constant across varying fps.
+        """
+
+        if t - self._last_update > self._delay:
+            return True
+
+    def update(self, t):
+        """
+        Update the sprite animation if enough time has passed.
+        t would be pygame.time.get_ticks() passed from the caller.
+        Call this each game tick, either manually if this sprite is stored in a list,
+        or if you keep sprites in a PyGame.Group object it will be called for you when you
+        issue the Group.draw() method.
+        """
+
+        if self.canupdate(t):
+            self._last_update = t
+            if self._hasframes:
+                self._frame += 1
+                if self._frame >= len(self._images):
+                    self._frame = 0
+                    if self.loop > 0:
+                        self.loop -= 1
+                    if self.loop == 0:
+                        self._hasframes = False
+                        self._frame = -1
+                self.image = self._images[self._frame]
+
+
+
 # KEEP THIS AS THE MOVEMENT IS NICE FOR THE MOVING MOONBASE SPRITES
 class Sprite(pygame.sprite.Sprite):
     """
